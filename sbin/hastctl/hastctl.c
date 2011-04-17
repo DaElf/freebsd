DaElf/freebsd

commit 3156c1ef3eab8e1fd2b96843abf3090b407fd097

In hast_proto_recv() remove unnecessary check. The size is checked later in hast_proto_recv_data().
Approved by:	pjd (mentor)
MFC after:	1 week


git-svn-id: svn://svn.freebsd.org/base/head@220523 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

sbin/hastd/hast_proto.c
@@ -225,17 +225,12 @@ hast_proto_recv(const struct hast_resource *res, struct proto_conn *conn,
     struct nv **nvp, void *data, size_t size)
 {
 	struct nv *nv;
-	size_t dsize;
 	int ret;
 
 	ret = hast_proto_recv_hdr(conn, &nv);
 	if (ret < 0)
 		return (ret);
-	dsize = nv_get_uint32(nv, "size");
-	if (dsize == 0)
-		(void)nv_set_error(nv, 0);
-	else
-		ret = hast_proto_recv_data(res, conn, nv, data, size);
+	ret = hast_proto_recv_data(res, conn, nv, data, size);
 	if (ret < 0)
 		nv_free(nv);
 	else

commit 09c632d7cdd3671efd76db59e1309a24c7526868

Remove hast_proto_recv(). It was used only in one place, where hast_proto_recv_hdr() may be used. This also fixes the issue (introduced by r220523) with hastctl, which crashed on assert in hast_proto_recv_data().
Suggested and approved by:	pjd (mentor)


git-svn-id: svn://svn.freebsd.org/base/head@220744 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

sbin/hastctl/hastctl.c
@@ -491,7 +491,7 @@ main(int argc, char *argv[])
 	}
 	nv_free(nv);
 	/* ...and receive reply. */
-	if (hast_proto_recv(NULL, controlconn, &nv, NULL, 0) < 0) {
+	if (hast_proto_recv_hdr(controlconn, &nv) < 0) {
 		pjdlog_exit(EX_UNAVAILABLE,
 		    "cannot receive reply from hastd via %s",
 		    cfg->hc_controladdr);

sbin/hastd/hast_proto.c
@@ -219,21 +219,3 @@ end:
 		free(dptr);
 	return (ret);
 }
-
-int
-hast_proto_recv(const struct hast_resource *res, struct proto_conn *conn,
-    struct nv **nvp, void *data, size_t size)
-{
-	struct nv *nv;
-	int ret;
-
-	ret = hast_proto_recv_hdr(conn, &nv);
-	if (ret < 0)
-		return (ret);
-	ret = hast_proto_recv_data(res, conn, nv, data, size);
-	if (ret < 0)
-		nv_free(nv);
-	else
-		*nvp = nv;
-	return (ret);
-}

sbin/hastd/hast_proto.h
@@ -39,8 +39,6 @@
 
 int hast_proto_send(const struct hast_resource *res, struct proto_conn *conn,
     struct nv *nv, const void *data, size_t size);
-int hast_proto_recv(const struct hast_resource *res, struct proto_conn *conn,
-    struct nv **nvp, void *data, size_t size);
 int hast_proto_recv_hdr(const struct proto_conn *conn, struct nv **nvp);
 int hast_proto_recv_data(const struct hast_resource *res,
     struct proto_conn *conn, struct nv *nv, void *data, size_t size);