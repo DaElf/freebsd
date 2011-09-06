eea1db1d6fa5e22278cc49034b8100419a581276
Russell Cattelan <cattelan@thebarn.com>
2011-09-06 10:25:30 -0500

    Very rough outline of kload.
    First add a kload syscall.
    This will be used to copy in the needed bits.
    	kernel, kenv, modules
    
    The flags will be expanded to provide various behaviors.
    	reboot, unload, etc
    
    kern_kload.c / kload.h
    	currently can copy in pages for "x" number of segments
    	Right now the userpace kload program is just copying
     	the entire memory block in. Which may prove to be the
    	the simplest way, but may be more pages than needed.
    	In the future it can copy in just the number of required
    	segments.

---
+++ sys/sys/kload.h
@@ -0,0 +1,40 @@
+/* Russell Cattelan Digital Elves Inc 2011 */
+
+
+#define KLOAD_LOAD		 0
+#define KLOAD_REBOOT		(1 << 0 )
+#define KLOAD_EXEC		(1 << 0 )
+#define KLOAD_TEST_RELOCATE	(1 << 10)
+
+struct kload_segment {
+	void		       *k_buf;
+	size_t			k_memsz;
+};
+	
+struct kload {
+	/* This may only be partially filled in.
+	 * OR could create our own segment describtor structure */
+//	struct Elf_Phdr phdr[10];
+	struct kload_segment khdr[10];
+	int num_hdrs;
+};
+
+//typedef unsigned long kload_item_t;
+#define KLOAD_DESTINATION  0x1
+#define KLOAD_INDIRECT     0x2
+#define KLOAD_DONE         0x4
+#define KLOAD_SOURCE       0x8
+
+struct kload_items {
+	struct vm_object *kload_obj;
+	
+	unsigned long head;
+	unsigned long *last_item;
+	unsigned long *item;
+  
+	
+	unsigned long flags;  /* not used yet */
+};
+
+
+
