911ed5436bc708c1b5bec77d761270c96cac8166
Russell Cattelan <cattelan@digitalelves.com>
2011-10-24 13:39:23 -0500

    Debugging for console setup in userboot

--- sys/boot/common/console.c
+++ sys/boot/common/console.c
@@ -54,6 +54,7 @@ cons_probe(void)
     
     /* Do all console probes */
     for (cons = 0; consoles[cons] != NULL; cons++) {
+      printf("%s cons %d c_probe %p\n",__FUNCTION__,cons,consoles[cons]->c_probe);
 	consoles[cons]->c_flags = 0;
  	consoles[cons]->c_probe(consoles[cons]);
     }
@@ -71,6 +72,7 @@ cons_probe(void)
 
     /* Check to see if a console preference has already been registered */
     prefconsole = getenv("console");
+    printf("%s prefconsole %s\n",__FUNCTION__,prefconsole);
     if (prefconsole != NULL)
 	prefconsole = strdup(prefconsole);
     if (prefconsole != NULL) {
