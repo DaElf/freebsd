7c2f217ecffcfc32c8f5f56bb452419ca9409aa7
Russell Cattelan <cattelan@thebarn.com>
2017-11-16 19:47:01 -0600

    merge fixup

--- stand/userboot/test/test.c
+++ stand/userboot/test/test.c
@@ -379,12 +379,6 @@ test_getenv(void *arg, int idx)
 	return (vars[idx]);
 }
 
-static int
-test_buildsmap(void *arg, void **smap_void, size_t *outlen)
-{
-	return (0);
-}
-
 struct loader_callbacks cb = {
 	.putc = test_putc,
 	.getc = test_getc,
@@ -414,7 +408,6 @@ struct loader_callbacks cb = {
         .getmem = test_getmem,
 
 	.getenv = test_getenv,
-	.buildsmap = test_buildsmap,
 };
 
 void
@@ -478,5 +471,5 @@ main(int argc, char** argv)
 	term.c_lflag &= ~(ICANON|ECHO);
 	tcsetattr(0, TCSAFLUSH, &term);
 
-	func(&cb, NULL, USERBOOT_VERSION_4, disk_fd >= 0);
+	func(&cb, NULL, USERBOOT_VERSION_3, disk_fd >= 0);
 }
